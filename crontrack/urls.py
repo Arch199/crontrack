5e1609df3ef77e5c7996b8ab5dc1b9a69144d880
James Dyer <jamesdyer199@gmail.com>
2019-01-27 14:49:10 +1000

    Add password change feature

--- crontrack/urls.py
+++ crontrack/urls.py
@@ -22,7 +22,11 @@ urlpatterns = [
 	
 	path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 	path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-	path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+	path(
+		'accounts/password_change/',
+		auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('crontrack:password_change_done')),
+		name='password_change',
+	),
 	path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 	path(
 		'accounts/password_reset/',
